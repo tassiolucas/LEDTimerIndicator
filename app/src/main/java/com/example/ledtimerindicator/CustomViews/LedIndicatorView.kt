cee6649e350b4232393b6189510b38c318f8a41c
Tassio Lucas <tassiolucas@live.com>
2019-05-03 11:28:45 -0300

    Constroi o LED de segmentos

--- app/src/main/java/com/example/ledtimerindicator/CustomViews/LedIndicatorView.kt
+++ app/src/main/java/com/example/ledtimerindicator/CustomViews/LedIndicatorView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
@@ -19,6 +21,11 @@ class LedIndicatorView : View {
     lateinit var ledSix: Paint
     lateinit var ledSeven: Paint
 
+    var ledStartX: Float = 0.0F
+    var ledStartY: Float = 0.0F
+    var ledStopX: Float = 0.0F
+    var ledStopY: Float = 0.0F
+
     constructor(context: Context) : super(context) {
         init(context)
     }
@@ -33,6 +40,8 @@ class LedIndicatorView : View {
 
     private fun init(context: Context) {
         invalidate()
+
+        this.setBackgroundColor(resources.getColor(R.color.hint_imput))
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
@@ -51,13 +60,15 @@ class LedIndicatorView : View {
         ledSix = Paint()
         ledSeven = Paint()
 
-        ledOne.style = Paint.Style.FILL
-        ledTwo.style = Paint.Style.FILL
-        ledThree.style = Paint.Style.FILL
-        ledFour.style = Paint.Style.FILL
-        ledFive.style = Paint.Style.FILL
-        ledSix.style = Paint.Style.FILL
-        ledSeven.style = Paint.Style.FILL
+        ledOne.style = Paint.Style.FILL_AND_STROKE
+        ledTwo.style = Paint.Style.FILL_AND_STROKE
+        ledThree.style = Paint.Style.FILL_AND_STROKE
+        ledFour.style = Paint.Style.FILL_AND_STROKE
+        ledFive.style = Paint.Style.FILL_AND_STROKE
+        ledSix.style = Paint.Style.FILL_AND_STROKE
+        ledSeven.style = Paint.Style.FILL_AND_STROKE
+
+        ledOne.strokeWidth = 25F
 
         ledOne.alpha = 255
         ledTwo.alpha = 255
@@ -79,12 +90,80 @@ class LedIndicatorView : View {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        var rect = Rect(100, 500, 600, 900)
-
-        canvas!!.drawRect(rect, ledOne)
+        turnLedOne(canvas!!, true)
+        turnLedTwo(canvas!!, true)
+        turnLedThree(canvas!!, true)
+        turnLedFour(canvas!!, true)
+        turnLedFive(canvas!!, true)
+        turnLedSix(canvas!!, true)
+        turnLedSeven(canvas!!, true)
 
         invalidate()
     }
+
+    fun turnLedOne(canvas: Canvas, isOn: Boolean) {
+        ledStartX = ((width / 22.5) * 2).toFloat()
+        ledStartY = ((height / 22.5) * 2).toFloat()
+        ledStopX = ((width / 22.5) * 2).toFloat()
+        ledStopY = ((height / 2) - (height / 22.5)).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedTwo(canvas: Canvas, isOn: Boolean) {
+        ledStartX = ((width / 22.5) * 4).toFloat()
+        ledStartY = (height / 22.5).toFloat()
+        ledStopX = (width.toFloat() - ((width.toFloat() / 22.5) * 4)).toFloat()
+        ledStopY = (height / 22.5).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedThree(canvas: Canvas, isOn: Boolean) {
+        ledStartX = (width.toFloat() - ((width.toFloat() / 22.5) * 2)).toFloat()
+        ledStartY = ((height / 22.5) * 2).toFloat()
+        ledStopX = (width - (width.toFloat() / 22.5) * 2).toFloat()
+        ledStopY = ((height / 2) - (height / 22.5)).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedFour(canvas: Canvas, isOn: Boolean) {
+        ledStartX = (height / 22.5).toFloat() // ((height / 2) + (height / 22.5)).toFloat()
+        ledStartY = ((height / 2) + (height / 22.5)).toFloat()
+        ledStopX = (height / 22.5).toFloat()
+        ledStopY = (height - ((height / 22.5) * 2)).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedFive(canvas: Canvas, isOn: Boolean) {
+        ledStartX = ((width / 22.5) * 4).toFloat()
+        ledStartY = (height / 2).toFloat()
+        ledStopX = (width - (width / 22.5) * 4).toFloat()
+        ledStopY = (height / 2).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedSix(canvas: Canvas, isOn: Boolean) {
+        ledStartX = ((width / 22.5) * 4).toFloat()
+        ledStartY = (height - (height / 22.5)).toFloat()
+        ledStopX = (width - ((width / 22.5) * 4)).toFloat()
+        ledStopY = (height - (height / 22.5)).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
+    fun turnLedSeven(canvas: Canvas, isOn: Boolean) {
+        ledStartX = (width - ((width / 22.5) * 2)).toFloat()
+        ledStartY = ((height / 2) + (height / 22.5)).toFloat()
+        ledStopX = (width - ((width / 22.5) * 2)).toFloat()
+        ledStopY = (height - ((height / 22.5) * 2)).toFloat()
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+    }
+
 
 
 
