5d78085852e732a9237a3a5a791557f4951f4ecb
Tassio Lucas <tassiolucas@live.com>
2019-05-03 13:57:27 -0300

    Inclui logica de funcionamento do display de LED

--- app/src/main/java/com/example/ledtimerindicator/CustomViews/LedIndicatorView.kt
+++ app/src/main/java/com/example/ledtimerindicator/CustomViews/LedIndicatorView.kt
@@ -3,9 +3,6 @@ package com.example.ledtimerindicator.CustomViews
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Rect
-import android.os.Build
-import android.support.annotation.RequiresApi
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
@@ -13,7 +10,7 @@ import com.example.ledtimerindicator.R
 
 class LedIndicatorView : View {
 
-    lateinit var ledOne: Paint
+    lateinit var ledPaint: Paint
     lateinit var ledTwo: Paint
     lateinit var ledThree: Paint
     lateinit var ledFour: Paint
@@ -52,119 +49,196 @@ class LedIndicatorView : View {
     }
 
     private fun initPaints(widthMeasure: Float, heightMeasure: Float) {
-        ledOne = Paint()
-        ledTwo = Paint()
-        ledThree = Paint()
-        ledFour = Paint()
-        ledFive = Paint()
-        ledSix = Paint()
-        ledSeven = Paint()
-
-        ledOne.style = Paint.Style.FILL_AND_STROKE
-        ledTwo.style = Paint.Style.FILL_AND_STROKE
-        ledThree.style = Paint.Style.FILL_AND_STROKE
-        ledFour.style = Paint.Style.FILL_AND_STROKE
-        ledFive.style = Paint.Style.FILL_AND_STROKE
-        ledSix.style = Paint.Style.FILL_AND_STROKE
-        ledSeven.style = Paint.Style.FILL_AND_STROKE
-
-        ledOne.strokeWidth = 25F
-
-        ledOne.alpha = 255
-        ledTwo.alpha = 255
-        ledThree.alpha = 255
-        ledFour.alpha = 255
-        ledFive.alpha = 255
-        ledSix.alpha = 255
-        ledSeven.alpha = 255
-
-        ledOne.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledTwo.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledThree.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledFour.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledFive.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledSix.color = ContextCompat.getColor(context, R.color.colorAccent)
-        ledSeven.color = ContextCompat.getColor(context, R.color.colorAccent)
+        ledPaint = Paint()
+        ledPaint.style = Paint.Style.FILL_AND_STROKE
+        ledPaint.strokeWidth = 25F
+        ledPaint.alpha = 255
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        turnLedOne(canvas!!, true)
-        turnLedTwo(canvas!!, true)
-        turnLedThree(canvas!!, true)
-        turnLedFour(canvas!!, true)
-        turnLedFive(canvas!!, true)
-        turnLedSix(canvas!!, true)
-        turnLedSeven(canvas!!, true)
+        setScreenValue(canvas!!, 9)
 
         invalidate()
     }
 
-    fun turnLedOne(canvas: Canvas, isOn: Boolean) {
+    private fun setLedOne(canvas: Canvas, isOn: Boolean) {
         ledStartX = ((width / 22.5) * 2).toFloat()
         ledStartY = ((height / 22.5) * 2).toFloat()
         ledStopX = ((width / 22.5) * 2).toFloat()
         ledStopY = ((height / 2) - (height / 22.5)).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedTwo(canvas: Canvas, isOn: Boolean) {
+    private fun setLedTwo(canvas: Canvas, isOn: Boolean) {
         ledStartX = ((width / 22.5) * 4).toFloat()
         ledStartY = (height / 22.5).toFloat()
         ledStopX = (width.toFloat() - ((width.toFloat() / 22.5) * 4)).toFloat()
         ledStopY = (height / 22.5).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedThree(canvas: Canvas, isOn: Boolean) {
+    private fun setLedThree(canvas: Canvas, isOn: Boolean) {
         ledStartX = (width.toFloat() - ((width.toFloat() / 22.5) * 2)).toFloat()
         ledStartY = ((height / 22.5) * 2).toFloat()
         ledStopX = (width - (width.toFloat() / 22.5) * 2).toFloat()
         ledStopY = ((height / 2) - (height / 22.5)).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedFour(canvas: Canvas, isOn: Boolean) {
-        ledStartX = (height / 22.5).toFloat() // ((height / 2) + (height / 22.5)).toFloat()
+    private fun setLedFour(canvas: Canvas, isOn: Boolean) {
+        ledStartX = (height / 22.5).toFloat()
         ledStartY = ((height / 2) + (height / 22.5)).toFloat()
         ledStopX = (height / 22.5).toFloat()
         ledStopY = (height - ((height / 22.5) * 2)).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedFive(canvas: Canvas, isOn: Boolean) {
+    private fun setLedFive(canvas: Canvas, isOn: Boolean) {
         ledStartX = ((width / 22.5) * 4).toFloat()
         ledStartY = (height / 2).toFloat()
         ledStopX = (width - (width / 22.5) * 4).toFloat()
         ledStopY = (height / 2).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedSix(canvas: Canvas, isOn: Boolean) {
+    private fun setLedSix(canvas: Canvas, isOn: Boolean) {
         ledStartX = ((width / 22.5) * 4).toFloat()
         ledStartY = (height - (height / 22.5)).toFloat()
         ledStopX = (width - ((width / 22.5) * 4)).toFloat()
         ledStopY = (height - (height / 22.5)).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
-    fun turnLedSeven(canvas: Canvas, isOn: Boolean) {
+    private fun setLedSeven(canvas: Canvas, isOn: Boolean) {
         ledStartX = (width - ((width / 22.5) * 2)).toFloat()
         ledStartY = ((height / 2) + (height / 22.5)).toFloat()
         ledStopX = (width - ((width / 22.5) * 2)).toFloat()
         ledStopY = (height - ((height / 22.5) * 2)).toFloat()
 
-        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledOne)
+        setLedState(isOn)
+
+        canvas.drawRoundRect(ledStartX, ledStartY, ledStopX, ledStopY, 10F, 10F, ledPaint)
     }
 
+    private fun setLedState(isOn: Boolean) {
+        if (isOn)
+            ledPaint.color = ContextCompat.getColor(context, R.color.colorAccent)
+        else
+            ledPaint.color = ContextCompat.getColor(context, R.color.colorAccentDark)
+    }
 
-
-
+    internal fun setScreenValue(canvas: Canvas, value: Int) {
+        when(value) {
+            0 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, true)
+                setLedFive(canvas, false)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+            1 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, false)
+                setLedThree(canvas, false)
+                setLedFour(canvas, true)
+                setLedFive(canvas, false)
+                setLedSix(canvas, false)
+                setLedSeven(canvas, false)
+            }
+            2 -> {
+                setLedOne(canvas, false)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, true)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, false)
+            }
+            3 -> {
+                setLedOne(canvas, false)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, false)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+            4 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, false)
+                setLedThree(canvas, true)
+                setLedFour(canvas, false)
+                setLedFive(canvas, true)
+                setLedSix(canvas, false)
+                setLedSeven(canvas, true)
+            }
+            5 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, false)
+                setLedFour(canvas, false)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+            6 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, false)
+                setLedFour(canvas, true)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+            7 -> {
+                setLedOne(canvas, false)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, false)
+                setLedFive(canvas, false)
+                setLedSix(canvas, false)
+                setLedSeven(canvas, true)
+            }
+            8 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, true)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+            9 -> {
+                setLedOne(canvas, true)
+                setLedTwo(canvas, true)
+                setLedThree(canvas, true)
+                setLedFour(canvas, false)
+                setLedFive(canvas, true)
+                setLedSix(canvas, true)
+                setLedSeven(canvas, true)
+            }
+        }
+    }
 }
